7c30f8626446e8a100c7e62ace4ad5b422548dd7
Enrique Salazar <EnriqueSa7@gmail.com>
2019-07-07 01:03:03 -0500

    404 not found functionalities

--- src/app/events/event-details/event-route-activator.service.ts
+++ src/app/events/event-details/event-route-activator.service.ts
@@ -1,15 +1,16 @@
-import { CanActivate } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-
+import { EventService } from 'src/app/shared/event.service';
 
 @Injectable({ providedIn: 'root' })
-
 export class EventRouteActivator implements CanActivate {
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private router: Router) {}
 
-  }
-
-  canActivate( route: ActivatedRouteSnapshot) {
-
+  canActivate(route: ActivatedRouteSnapshot) {
+    const eventExist = !!this.eventService.getEvent(+route.params.id);
+    if (!eventExist) {
+      this.router.navigate(['404']);
+    }
+    return eventExist;
   }
 }
